d2d5315840a8d1af4187ab1f5cc1f17e754f645a
sajid <sajidsajidsyed538@gmail.com>
2025-12-09 18:51:53 +0500

    its my sss name project

--- sss/sss/src/app/about/page.tsx
+++ sss/sss/src/app/about/page.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      my frist page
-      
+      its about page
     </div>
   )
 }
--- sss/sss/src/app/page.tsx
+++ sss/sss/src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      contact us 
+    its home page  
     </div>
   )
 }
