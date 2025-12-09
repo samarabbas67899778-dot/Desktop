8fa2f3bdd4cc273b9b9220c2832bedf8d242bcc8
sajid <sajidsajidsyed538@gmail.com>
2025-12-09 17:15:19 +0500

    my frist project

---
+++ vibe3/vibe3/src/app/abot/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      its about my page
+    </div>
+  )
+}
+
+export default page
---
+++ vibe3/vibe3/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     allah o akbar 
+    </div>
+  )
+}
+
+export default page
