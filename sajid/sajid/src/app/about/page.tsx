03b2e659959e57d2987f1329f397b90033c62b54
sajid <sajidsajidsyed538@gmail.com>
2025-12-16 22:08:56 +0500

    sajid

--- sajid/sajid/src/app/about/page.tsx
+++ sajid/sajid/src/app/about/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     <div>
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sajid.jpg" alt="" width="384" height="512"/>
-  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+  <div className="pt-6 md:p-8 text.middle md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
@@ -15,10 +15,10 @@ const page = () => {
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+        Sajid syed
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
+        Staff Engineer, 
       </div>
     </figcaption>
   </div>
