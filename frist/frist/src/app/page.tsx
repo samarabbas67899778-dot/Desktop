b97fd80d542cc3738d78ae96ac749cddbf8a5fdf
syeddsajidshah672 <syeddsajidshah672@gmail.com>
2025-12-18 18:48:38 +0500

    frist

--- frist/frist/src/app/page.tsx
+++ frist/frist/src/app/page.tsx
@@ -8,17 +8,17 @@ const page = () => {
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
+         almighty allah is geatest
+        
+
       </p>
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
+        Sajid syed
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
+        student
       </div>
     </figcaption>
   </div>
