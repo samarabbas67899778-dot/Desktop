e90eef125b0920f187163a13dfdf3f8f58e0b8a5
syeddsajidshah672 <syeddsajidshah672@gmail.com>
2025-12-18 12:36:40 +0500

    marhaba

--- marhaba/marhaba/src/app/page.tsx
+++ marhaba/marhaba/src/app/page.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sajid.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
-      <p className="text-lg font-large">
-        i am sajid syed village adlana tehsil bhawana dist chiniot may almighty 
-    
-  
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
       </p>
     </blockquote>
-    <figcaption className="font-large">
+    <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
         Sajid syed
       </div>
@@ -23,7 +23,6 @@ const page = () => {
     </figcaption>
   </div>
 </figure>
-
     </div>
   )
 }
