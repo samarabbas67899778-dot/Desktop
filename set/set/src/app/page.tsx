83dcdf82d63c5e99f6ee056f73efce43e732d10e
syeddsajidshah672 <syeddsajidshah672@gmail.com>
2025-12-19 07:07:09 +0500

    set

---
+++ set/set/src/app/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-100 h-60 md:w-70md:h-auto md: rounded-full mx-auto" src="/sajid.jpg" alt="" width="384" height="512"/>
+  <div className="pt-2 md:p-8 text-left md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-large">
+      oh almighty allah bless us the power of wisdom and power of deep thinkings ,allah ismthe most mercifull and magnificient
+      i am advo hafiz sajid syed s/o syed altaf hussain shah village adlana teh bhawana dist chiniot
+        
+        
+      </p>
+    </blockquote>
+    <figcaption className="extra-large">
+      <div className="text-sky-500 dark:text-sky-400">
+        Sajid syed
+      </div>
+      <div className="text-slate-900 dark:text-slate-500">
+        Staff Engineer
+      </div>
+    </figcaption> 
+  </div>
+</figure>
+    </div>
+  )
+}
+
+export default page
