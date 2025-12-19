ba4234243e7699d8c0ffaf16e49d3a5515ca271b
syeddsajidshah672 <syeddsajidshah672@gmail.com>
2025-12-19 06:08:28 +0500

    frist

---
+++ fast/fast/src/app/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-70 h-80 md:w-60 md:h-auto md:rounded-none rounded-full mx-auto" src="/sajid.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-large">
+        almighty allah is the greatwst oh almighty allh nless the power of wisdom ,tranquality, broad vision 
+        and peace of mind may almighty allah bless all of us
+        
+        
+      </p>
+    </blockquote>
+    <figcaption className="font-extra large">
+      <div className="text-sky-800 dark:text-sky-400">
+        Sajd syed
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Student
+      </div>
+    </figcaption>
+  </div>
+</figure>
+
+    </div>
+  )
+}
+
+export default page
